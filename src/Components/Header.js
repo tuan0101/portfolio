8917e79180820b99886f11b4120a465542362320
Tuan Pham <tuan.programmer0101@gmail.com>
2020-12-18 00:03:41 -0500

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/header.css';
+
+function Header() {
+    return (
+        <header id="home">
+            Header
+        </header>
+    )
+}
+
+export default Header
