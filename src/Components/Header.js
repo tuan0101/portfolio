b9a45034d8dba7e123a85e89e671c10068c2510b
Tuan Pham <tuan.programmer0101@gmail.com>
2020-12-18 13:35:19 -0500

    Nav Bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,50 @@ import '../css/header.css';
 function Header() {
     return (
         <header id="home">
-            Header
+            <nav id="nav-wrap">
+
+                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+                <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+                <ul id="nav" className="nav">
+                    <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                    <li><a className="smoothscroll" href="#about">About</a></li>
+                    <li><a className="smoothscroll" href="#resume">Resume</a></li>
+                    <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+                    <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                    <li><a className="smoothscroll" href="#contact">Contact</a></li>
+                </ul>
+
+            </nav>
+
+            <div className="banner">
+                <div className="banner-text">
+                    <h1 className="responsive-headline">
+                        I'm <span className="name">Tuan Pham</span>
+                        <span className="dot">.</span></h1>
+                    <h2>I'm a Software Developer.</h2>
+                    <hr />
+                    <a className="smoothscroll" href="#about">
+                        <div className="view-more">
+                            {/* {networks} */}
+                     View My Work
+                     <span className="scroll-icon "><i className="icon-down-circle"></i></span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <p className="scrolldown">
+                <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+            </p>
+
+            <div className="front-header"></div>
+            <div className="header-background"></div>
+            <div className="stars"></div>
+            <div className="stars1"></div>
+            <div className="stars2"></div>
+            <div className="stars3"></div>
+            <div className="twinkling"></div>
         </header>
     )
 }
