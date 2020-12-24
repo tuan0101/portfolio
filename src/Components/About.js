191f45cd21c9ef2ce47b3e5e1d49254ca517bbe7
Tuan Pham <tuan.programmer0101@gmail.com>
2020-12-24 14:10:12 -0500

    Finished About Section

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,6 +4,9 @@ import React from 'react'
 function About({ data, main }) {
     // var profilepic = "images/" + main.image;
 
+    if (main) {
+        var bio = main.bio;
+    }
     if (data) {
         var skills = data.skills.map(function (skills) {
             const className = 'bar-expand ' + skills.name.toLowerCase();
@@ -23,22 +26,25 @@ function About({ data, main }) {
     return (
         <section id="about">
             <div className="soft-skill">
-                <div>
-                    <div id="chevron"><i className="fab fa-d-and-d-beyond fa-3x" style={{ 'top': '-5px' }}></i></div>
-                    <div className="each-skill">Give more than what's expected</div>
-                </div>
-                <div>
-                    <div id="chevron"><i className="far fa-star fa-2x"></i></div>
-                    <div className="each-skill">Set high standards</div>
-                </div>
-                <div>
-                    <div id="chevron"><i className="fas fa-rocket fa-2x"></i></div>
-                    <div className="each-skill">Embrace accountability</div>
-                </div>
-                <div>
-                    <div id="chevron"><i className="fas fa-hands fa-2x"></i></div>
-                    <div className="each-skill">Care deeply</div>
-                </div>
+                <div className="soft-skill1">
+                    <div className="my-skill">
+                        <div id="chevron"><i className="fab fa-d-and-d-beyond fa-3x" style={{ 'top': '-5px' }}></i></div>
+                        <div className="each-skill">Give more than what's expected</div>
+                    </div>
+                    <div className="my-skill">
+                        <div id="chevron"><i className="far fa-star fa-2x"></i></div>
+                        <div className="each-skill">Set high standards</div>
+                    </div></div>
+                <div className="soft-skill2">
+
+                    <div className="my-skill">
+                        <div id="chevron"><i className="fas fa-rocket fa-2x"></i></div>
+                        <div className="each-skill">Embrace accountability</div>
+                    </div>
+                    <div className="my-skill">
+                        <div id="chevron"><i className="fas fa-hands fa-2x"></i></div>
+                        <div className="each-skill">Care deeply</div>
+                    </div></div>
             </div>
 
             <div className="row">
@@ -55,6 +61,8 @@ function About({ data, main }) {
                         <div data-mode="green" id="green-mode" className="theme-dot"></div>
                         <div data-mode="purple" id="purple-mode" className="theme-dot"></div>
                     </div>
+                    <h3>Hi. I'm Tuan, nice to meet you!</h3>
+                    <p>{bio}</p>
                 </div>
                 <div className="column-right">
                     <div className="bars">
