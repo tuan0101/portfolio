fc829e95f26fb48ce797477400d559ab71f32dcc
Tuan Pham <tuan.programmer0101@gmail.com>
2021-01-12 15:21:05 -0500

    Updates

--- js/DynamicTheme.js
+++ js/DynamicTheme.js
@@ -20,19 +20,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = '/css/default.css'
+		document.getElementById('theme-style').href = '/portfolio/css/default.css'
 	}
 
 	if(mode == 'blue'){
-        document.getElementById('theme-style').href = '/css/blue.css'
+        document.getElementById('theme-style').href = '/portfolio/css/blue.css'
 	}
 
 	if(mode == 'green'){
-		document.getElementById('theme-style').href = '/css/green.css'
+		document.getElementById('theme-style').href = '/portfolio/css/green.css'
 	}
 
 	if(mode == 'purple'){
-		document.getElementById('theme-style').href = '/css/purple.css'
+		document.getElementById('theme-style').href = '/portfolio/css/purple.css'
 	}
 
 	localStorage.setItem('theme', mode)
