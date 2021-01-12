ba5525cf48ee11c090be50b8545c1c96c570da7b
Tuan Pham <tuan.programmer0101@gmail.com>
2021-01-12 11:47:53 -0500

    Updates

---
+++ js/init.js
@@ -0,0 +1,98 @@
+/*-----------------------------------------------------------------------------------
+/* Init JS
+-----------------------------------------------------------------------------------*/
+
+jQuery(document).ready(function ($) {
+
+    /*----------------------------------------------------*/
+    /* Smooth Scrolling
+    ------------------------------------------------------ */
+
+    // $('.smoothscroll').on('click', function (e) {
+    //     e.preventDefault();
+
+    //     var target = this.hash,
+    //         $target = $(target);
+
+    //     $('html, body').stop().animate({
+    //         'scrollTop': $target.offset().top
+    //     }, 800, 'swing', function () {
+    //         window.location.hash = target;
+    //     });
+    // });
+
+
+    /*----------------------------------------------------*/
+    /* Highlight the current section in the navigation bar
+    ------------------------------------------------------*/
+
+    var sections = $("section");
+    var navigation_links = $("#nav-wrap a");
+
+    sections.waypoint({
+
+        handler: function (event, direction) {
+
+            var active_section;
+
+            active_section = $(this);
+            if (direction === "up") active_section = active_section.prev();
+
+            console.log('active_section', active_section);
+            var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+            console.log('active_link', active_link);
+            navigation_links.parent().removeClass("current");
+            active_link.parent().addClass("current");  // activate orange color
+
+        },
+        offset: '50%'
+
+    });
+
+    /*----------------------------------------------------*/
+    /*	Fade In/Out Primary Navigation
+    ------------------------------------------------------*/
+
+    $(window).on('scroll', function () {
+
+        var h = $('header').height();
+        var y = $(window).scrollTop();
+        var nav = $('#nav-wrap');
+
+        if ((y > h * .20) && (y < h*.95) && ($(window).outerWidth() > 768)) {
+            nav.fadeOut('fast');
+        }
+        else {
+            if (y < h * .20) {
+                nav.removeClass('opaque').fadeIn('fast');
+            }
+            else {
+                nav.addClass('opaque').fadeIn('fast');
+            }
+        }
+
+    });
+
+    /*----------------------------------------------------*/
+    /*	Flexslider
+    /*----------------------------------------------------*/
+    $('.flexslider').flexslider({
+        namespace: "flex-",
+        controlsContainer: ".flex-container",
+        animation: 'slide',
+        controlNav: true,
+        directionNav: false,
+        smoothHeight: true,
+        slideshowSpeed: 7000,
+        animationSpeed: 600,
+        randomize: false,
+    });
+});
+
+
+
+
+
+
+
+
